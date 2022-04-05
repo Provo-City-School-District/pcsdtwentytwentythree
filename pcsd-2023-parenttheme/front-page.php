83a157826ab4833c465bfa2912e85cf7746c432f
Josh Espinoza <joshespi@gmail.com>
2022-04-05 00:10:30 -0600

    Create front-page.php

---
+++ pcsd-2023-parenttheme/front-page.php
@@ -0,0 +1,9 @@
+<?php
+  get_header(); // Call the theme’s header.php file.
+?>
+      <section id="pageContent">
+        
+      </section>
+<?php
+  get_footer(); // Call the theme’s footer.php file.
+?>
