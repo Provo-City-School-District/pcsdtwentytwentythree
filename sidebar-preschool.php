120ee314fde68c887a1783f63417d713105dea51
Josh Espinoza <joshespi@gmail.com>
2023-07-06 11:17:04 -0600

    WO107677

--- sidebar-preschool.php
+++ sidebar-preschool.php
@@ -1,11 +1,11 @@
 <aside id="rightSidebar" class="rightSidebar">
-	
-	<h3><a class="ext" href="https://drive.google.com/file/d/1zCvHG_Zzy-qX7JG8969mlMxCEadz13AO/view"><strong>Baby Bulldog Preschool (PHS)</strong></a></h3>
+	<h2>Contact Preschool</h2>
 	<?php
-	echo file_get_contents('https://provo.edu/directory_page/school-preschool-baby-bulldog/');
-	?>
-	<h3><strong>Timpview Preschool (THS)</strong></h3>
-	<?php
-	echo file_get_contents('https://provo.edu/directory_page/school-preschool-ths-preschool/');
+		echo "<h3>Sunrise</h3>";
+		echo file_get_contents('https://provo.edu/directory_page/school-preschool-sunrise/');
+		echo "<h3>Title I</h3>";
+		echo file_get_contents('https://provo.edu/directory_page/school-preschool-district-title-i/');
+		echo "<h3>Highschools</h3>";
+		echo file_get_contents('https://provo.edu/directory_page/school-preschool-ths-preschool/');
 	?>
 </aside>
