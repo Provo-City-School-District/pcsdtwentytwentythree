34d33d432856fd90554775de7af628bff272ae33
Josh Espinoza <joshespi@gmail.com>
2023-10-11 10:17:04 -0600

    made NDA pull from internal page rather then template

--- template-childNutrition-tiles.php
+++ template-childNutrition-tiles.php
@@ -166,28 +166,10 @@ get_header();
 
 		</section>
 		<aside class="fwContent">
-			<h2>Non-discrimination statement</h2>
-			<p>In accordance with Federal civil rights law and U.S. Department of Agriculture (USDA) civil rights regulations and policies, the USDA, its Agencies, offices, and employees, and institutions participating in or administering USDA programs are prohibited from discriminating based on race, color, national origin, sex, disability, age, or reprisal or retaliation for prior civil rights activity in any program or activity conducted or funded by USDA.</p>
-
-			<p>Persons with disabilities who require alternative means of communication for program information (e.g. Braille, large print, audiotape, American Sign Language, etc.), should contact the Agency (State or local) where they applied for benefits. Individuals who are deaf, hard of hearing or have speech disabilities may contact USDA through the Federal Relay Service at (800) 877-8339. Additionally, program information may be made available in languages other than English.</p>
-
-			<p>To file a program complaint of discrimination, complete the USDA Program Discrimination Complaint Form, (AD-3027) found online at: //www.ascr.usda.gov/complaint_filing_cust.html, and at any USDA office, or write a letter addressed to USDA and provide in the letter all of the information requested in the form. To request a copy of the complaint form, call (866) 632-9992. Submit your completed form or letter to USDA by:</p>
-			<ol>
-				<li><strong>mail</strong>:
-					<ul class="address">
-						<li>U.S. Department of Agriculture</li>
-						<li>Office of the Assistant Secretary for Civil Rights</li>
-						<li>1400 Independence Avenue, SW</li>
-						<li>Washington, D.C. 20250-9410</li>
-					</ul>
-				</li>
-				<li><Strong>fax</Strong>: (202) 690-7442</li>
-				<li><strong>email</strong>: program.intake@usda.gov</li>
-			</ol>
-			<p>This institution is an equal opportunity provider.</p>
-			<ul>
-				<li class="pdf"><a href="http://district.schoolnutritionandfitness.com/snaflib/files/And_Justice_for_all_poster.pdf">Non Discrimination Disclosure Download</a></li>
-			</ul>
+			<?php
+			$cn_nda = get_post(71667);
+			echo do_shortcode($cn_nda->post_content);
+			?>
 		</aside>
 
 	</div>
