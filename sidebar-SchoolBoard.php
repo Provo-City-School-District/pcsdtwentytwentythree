19b5dc13bf73c9652399c8dcd0a3bb3edda8b48f
Josh Espinoza <joshespi@gmail.com>
2023-12-01 14:01:53 -0700

    modification by Rob

--- sidebar-SchoolBoard.php
+++ sidebar-SchoolBoard.php
@@ -1,9 +1,15 @@
 <aside id="rightSidebar" class="rightSidebar">
 <ul class="imagelist">
+	<li>
+		<a href="https://provo.edu/category/news/pcsd-school-board/board-summary/">
+			<img src="//globalassets.provo.edu/image/icons/board-lt.svg" alt="logo" />
+			<span>Board Meeting Summaries</span>
+		</a>
+	</li>	
 		<li>
 			<a href="https://provo.edu/district-office-directory/email-form/?staff=68051">
 				<img src="//globalassets.provo.edu/image/icons/email.svg" alt="logo" />
-				<span>Email Entire School Board</span>
+				<span>Email All Board Members</span>
 			</a>
 		</li>	
 <h2>Board Member Contacts</h2>
