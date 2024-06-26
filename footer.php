71d2f78a108b7bb738fd0999132a81b7dde2da4d
Josh Espinoza <joshespi@gmail.com>
2024-06-26 14:35:41 -0600

    fixed footer menu

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
 		// set URL and other appropriate options
 		curl_setopt($globalfooter, CURLOPT_URL, 'https://globalassets.provo.edu/globalpages/ada-footer.php');
 		curl_setopt($globalfooter, CURLOPT_HEADER, 0);
+		curl_setopt($globalfooter, CURLOPT_SSL_VERIFYPEER, false);
 		// grab URL and pass it to the browser
 		curl_exec($globalfooter);
 		// close cURL resource, and free up system resources
@@ -18,7 +19,7 @@
 
 if (is_page('search-results')) {
 ?>
-<!-- <script type="text/javascript" src="//customer.cludo.com/scripts/bundles/search-script.min.js"></script> -->
+	<!-- <script type="text/javascript" src="//customer.cludo.com/scripts/bundles/search-script.min.js"></script> -->
 	<script>
 		var CludoSearch;
 		(function() {
@@ -36,7 +37,7 @@ if (is_page('search-results')) {
 			CludoSearch.init();
 		})();
 	</script>
-<!--[if lte IE 9]>
+	<!--[if lte IE 9]>
 	<script src="https://api.cludo.com/scripts/xdomain.js" slave="https://api.cludo.com/proxy.html" type="text/javascript"></script>
 	<![endif]-->
 <?php
